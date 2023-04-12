32d65a636fca58798e64a5e8565abaff5801bfc0
safwan <safwanazman017@gmail.com>
2023-04-12 18:33:33 +0800

    update

--- src/components/section/Section6.js
+++ src/components/section/Section6.js
@@ -2,6 +2,8 @@ import React,{useState,useEffect} from 'react'
 import { BottomModal } from '@/components/BottomModal';
 import { Ucapan } from '@/components/tabform/Ucapan';
 import callapi from '@/helper/axios';
+import {faHandPointRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 export const Section6 = () => {
@@ -33,21 +35,26 @@ export const Section6 = () => {
         <section className="h-[60vh] bg-center bg-cover pb-12">
             <div className=""  data-aos="fade-down" data-aos-delay="200">
                 <h1 className="pb-5 text-2xl font-medium text-center text-primary-500">Guestbook</h1>
-                <div className="flex flex-col px-6 py-4 mx-5 overflow-auto border border-black rounded-lg bg-primary-50 h-[50vh] relative text-center">
+                <div className="flex flex-col mx-5 overflow-auto border border-black rounded-lg bg-primary-50 h-[50vh] relative text-center">
                     {ucapanData.map((entry, index) => (
-                        <div key={index} className="py-4 border-b border-primary-200 ">
-                            <h1 className="text-xl font-semibold text-primary-600">{entry.nama}</h1>
-                            <h1 className="">{entry.ucapan}</h1>
+                        <div key={index}  className="px-6 py-4 ">
+                            <div className="py-4 border-b border-primary-200 ">
+                                <h1 className="text-xl font-semibold text-primary-600">{entry.nama}</h1>
+                                <h1 className="">{entry.ucapan}</h1>
+                            </div>
                         </div>
                     ))}
-                    <div className="sticky bottom-0">
+                    <div className="sticky bottom-0 px-2 py-2">
                         <button 
                             onClick={()=>{
                                 if(tab != '1'){
                                     setTab('1')
                                 }
                             }}
-                            className="w-full px-8 py-3 text-sm font-medium transition bg-gray-800 rounded text-primary-100 hover:scale-100 focus:outline-none focus:ring active:bg-gray-700" type="submit">
+                            className="flex items-center justify-center w-full px-8 py-3 text-sm font-medium transition bg-gray-800 rounded text-primary-100 hover:scale-100 focus:outline-none focus:ring active:bg-gray-700" type="submit">
+                            <div className="animate__animated animate__swing animate__infinite">
+                                <FontAwesomeIcon className="mr-3 text-xl text-white " icon={faHandPointRight} />
+                            </div>
                             Berikan Ucapan
                         </button>
                     </div>
