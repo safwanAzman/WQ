d289f0f636c1c1b14b7e422c167dad6e97337f42
safwan <safwanazman017@gmail.com>
2023-04-23 20:50:20 +0800

    disable auto zoom input in real device

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,11 +3,16 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
+      <Head>
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
     </Html>
   )
 }
